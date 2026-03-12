5f9b76d1fc9dea88d077a378efb387888faf0d9c
Rakhi Ahuja <168163967+rakhiahuja7@users.noreply.github.com>
2026-03-12 12:42:58 +0530

    Create only_write.py

---
+++ Day10/FIle_Handling/only_write.py
@@ -0,0 +1,21 @@
+# file = open('temp.txt', 'w+')
+# # file = open('temp1.txt', 'w')
+# # file.write('I am the first line!')
+
+# file.writelines([
+#     'first line \n', 
+#     'second line\n',
+#     'third line \n',
+#     'fourth line \n', 
+#     'fifth line\n',
+#     'sixth line ' 
+# ]
+# )
+# ## to make the python interpreter to point at a specific index, we use seek(index).
+# file.seek(0)
+# print(file.read())
+
+
+
+
+file.close()
